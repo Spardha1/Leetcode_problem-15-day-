92b08bb7c87d06bea157cbb1a8c1144c0f199ea8
Spardha Kumari <80118289+Spardha1@users.noreply.github.com>
2024-07-05 17:46:39 +0530

    Time: 57 ms (28.86%) | Memory: 48.7 MB (55.11%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+ 
+//here the object is returning the function and also throwing the eroor
+
+    return{
+        toBe : (val1) =>{
+            if(val===val1){
+                return true;
+            }
+            else{
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe : (val2) =>{
+            if(val !==val2){
+                return true;
+            }
+            else{
+                throw new Error("Equal");
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
